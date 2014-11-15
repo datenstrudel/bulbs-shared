480afb35af2d0cffc10f8419fb4a785f5ee6305f
Travis CI Release <twx1@gmx.de>
2014-11-15 16:06:49 +0000

    This commit comprises all failed and finally successful changes for travis automatic OSS release setup. [maven-release-plugin] prepare release bulbs-shared-1.0
    [maven-release-plugin] prepare for next development iteration
    
    Travis Release exec - Fix GPG signing profile, rollback to Snapshot 1.0 due to no signatures were generated on deployment
    
    Provide GPG passphrase directly in release:perform call
    
    Travis Release exec - Try/Error 27 - Specify maven-gpg-plugin version; move release scripts to specific folder; enhance settings.xml
    
    Travis Release exec - Try/Error 28 -Fix "gpg2 not found"
    
    [maven-release-plugin] prepare release bulbs-shared-1.0
    
    [maven-release-plugin] prepare for next development iteration
    
    Travis Release exec - Try/Error 29  Disable gpg agent usage; rollback to
    version 1.0-SNAPSHOT
    
    Travis Release exec - Try/Error 30 Use email as gpg keyname
    
    Travis Release exec - Try/Error 31 Provide gpg passphrase in cmd line call
    
    Travis Release exec - Try/Error 32 Hopefully fix gpg passphrase statement
    
    Travis Release exec - Try/Error 33 Hopefully fix gpg passphrase statement
    
    Travis Release exec - Try/Error 34 Hopefully fix gpg passphrase statement
    
    Travis Release exec - Try/Error 35 Fix encryption parameters
    
    Sets branch to be released to master. This commit finishes the release setup torture.
    
    [maven-release-plugin] prepare release bulbs-shared-1.0
    
    [maven-release-plugin] prepare for next development iteration

---
+++ ci-scripts/release-script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -ev
+BRANCH_TO_RELEASE="master"
+if [ "${TRAVIS_BRANCH}" = "ci/releaseTrigger" ]; then
+  git clone --depth=50 --branch=${BRANCH_TO_RELEASE} git://github.com/datenstrudel/bulbs-shared.git master
+  cd master
+  pwd | xargs echo "Operating in path: "
+  openssl aes-256-cbc -d -a -pass pass:${GPG_ENCR_KEY} -in ci-scripts/pubring.gpg.encr -out ci-scripts/local.pubring.gpg
+  openssl aes-256-cbc -d -a -pass pass:${GPG_ENCR_KEY} -in ci-scripts/secring.gpg.encr -out ci-scripts/local.secring.gpg
+  gpg --import ci-scripts/local.pubring.gpg
+  gpg --allow-secret-key-import --import ci-scripts/local.secring.gpg
+  gpg --list-keys
+  echo "Branch is "${TRAVIS_BRANCH}
+  git config --global user.email "twx1@gmx.de"
+  git config --global user.name "Travis CI Release"
+  git config credential.helper "store --file=.git/credentials"
+  echo "https://${TRAVIS_GITHUB_TK}:@github.com" > .git/credentials
+  cat .git/HEAD|xargs echo "Head is: "
+  echo "Starting Maven release... "
+  PASSPHRASE_STRING="-Darguments=-Dgpg.passphrase="${GPG_PASSPHRASE}
+  mvn -B release:clean release:prepare --settings ci-scripts/settings.xml
+  mvn -B release:perform ${PASSPHRASE_STRING} --settings ci-scripts/settings.xml
+fi
