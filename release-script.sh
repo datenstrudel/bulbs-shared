b871d39be54baed3bce2f7a98727614633a498aa
Datenstrudel <twx1@gmx.de>
2014-11-15 12:47:16 +0100

    Travis Release exec - Try/Error 11th. Puts shell commands in separate script file

---
+++ release-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ev
+
+if [ ${TRAVIS_BRANCH}=='ci/releaseConfig' ]; then
+  gpg --import local.pubring.gpg
+  gpg --allow-secret-key-import --import local.secring.gpg
+  gpg --list-keys
+  echo "Branch is "$TRAVIS_BRANCH
+  git config --global user.email "twx1@gmx.de"
+  git config --global user.name "Travis CI Release"
+  git config credential.helper "store --file=.git/credentials"
+  echo "https://${TRAVIS_GITHUB_TK}:@github.com" > .git/credentials
+  git reset --hard $TRAVIS_BRANCH
+  cat .git/HEAD|xargs echo "Head is: "
+  echo "Starting Maven release... "
+  mvn -B release:clean release:prepare --settings settings.xml
+  mvn release:perform --settings settings.xml
+fi
